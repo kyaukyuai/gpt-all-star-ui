2bb5daa26612c0f57aa0a7906248b3016a11147c
kyaukyuai <y.kakui@gmail.com>
2024-03-23 11:18:27 +0900

    Add environment variable for browser configuration

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ os.environ["LANGCHAIN_TRACING_V2"] = st.secrets["LANGCHAIN_TRACING_V2"]
 os.environ["LANGCHAIN_ENDPOINT"] = st.secrets["LANGCHAIN_ENDPOINT"]
 os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
 os.environ["LANGCHAIN_PROJECT"] = st.secrets["LANGCHAIN_PROJECT"]
+os.environ["BROWSER"] = st.secrets["BROWSER"]
 
 st_init()
 st.image("static/logo-wide.png", width=420)
